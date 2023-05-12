649f2fda3f2ec36f91d54e5ce92c9df24f112bc4
Rob Benton <rob.benton@mainstream-tech.com>
2023-05-12 11:32:20 -0500

    Renamed and reorganized files

--- Repository/Collation.cs
+++ Repository/Collation.cs
@@ -1,5 +1,17 @@
 namespace Repository;
 
+public sealed class Collation
+{
+    public required string Name { get; init; }
+
+    public required string Locale { get; init; }
+
+    public required string Provider { get; init; }
+
+    public required bool Deterministic { get; init; }
+}
+
+
 public sealed class Collations
 {
     public static readonly Collation UcaStrengthPrimaryCollation =
